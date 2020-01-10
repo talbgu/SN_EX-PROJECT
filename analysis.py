5d3eeb8fb2cb7ebee0c1aec65a682640bbdad413
tal <tal@post.bgu.ac.il>
2020-01-10 13:49:11 +0200

    filter nodes

--- analysis.py
+++ analysis.py
@@ -13,8 +13,7 @@ def load_graph(csv_name):
     :param path: path of file.
     :return:
     """
-
-    print "start"
+    print("start")
 
     graph = nx.Graph()
     graph.nodes(data=True)
@@ -25,46 +24,46 @@ def load_graph(csv_name):
         v = row[1]
 
         if u not in graph.nodes():
-            graph.add_node(u)
+            graph.add_node(u, {"degree": 0})
         if v not in graph.nodes():
-            graph.add_node(v)
+            graph.add_node(v, {"degree": 0})
 
-        graph.add_edge(u, v)  # , id=i[2]['id'], weight=i[2]['weight'])
+        graph.add_edge(u, v)
+        graph.node[u]["degree"] += 1
+        graph.node[v]["degree"] += 1
     return graph
 
 
-graph = load_graph("ENGB_edges.csv")
-print ("num of node:", len(graph.nodes()))
-print("num of edges:", len(graph.edges()))
+def main():
+    graph = load_graph("ENGB_edges.csv")
+    print("num of node:", len(graph.nodes()))
+    print("num of edges:", len(graph.edges()))
 
-edges = [edge for edge in graph.edges(data=True) if (edge[2]['weight'] > 2)]
 
-selected_nodes = set()
-for edge in edges:
-    selected_nodes.add(edge[0])
-    selected_nodes.add(edge[1])
-# len(selected_nodes)
-len(graph.nodes())
+    nodes = [n for n in graph.node if graph.node[n]['degree'] >= 5]
 
-# graph = graph.subgraph(selected_nodes)
-all_node = set()
-for n in graph.nodes():
-    all_node.add(n)
 
-graph.clear()
-for i in edges:
-    utag = i[0]
-    v_tag = i[1]
-    if u not in graph.nodes():
-        graph.add_node(u)
-    if v not in graph.nodes():
-        graph.add_node(v)
-    graph.add_edge(u, v, id=i[2]['id'], weight=i[2]['weight'])
-for n in all_node:
-    if n not in graph.nodes():
-        graph.add_node(n)
+    edges_tag = set()
+    for n in graph.edge():
+        all_node.add(n)
+    graph.clear()
+    for i in edges:
+        u_tag = i[0]
+        v_tag = i[1]
+        if u_tag not in graph.nodes():
+            graph.add_node(u_tag)
+        if v_tag not in graph.nodes():
+            graph.add_node(v_tag)
+        graph.add_edge(u_tag, v_tag, id=i[2]['id'], weight=i[2]['weight'])
+    for n in all_node:
+        if n not in graph.nodes():
+            graph.add_node(n)
 
-print ("num of node after filtering:", len(graph.nodes()))
-print("num of edges after filtering:", len(graph.edges()))
+    print("num of node after filtering:", len(graph.nodes()))
+    print("num of edges after filtering:", len(graph.edges()))
 
-print ("the density of the graph:", nx.density(graph))
+    print("the density of the graph:", nx.density(graph))
+
+
+if __name__ == '__main__':
+    main()
